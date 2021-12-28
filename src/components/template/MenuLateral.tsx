fe1fa8028d056542ec667eb303d91124316ffe72
Vinícius <euphonion@tutanota.com>
2021-12-27 23:28:50 -0300

    Título e logo

--- src/components/template/MenuLateral.tsx
+++ src/components/template/MenuLateral.tsx
@@ -1,9 +1,19 @@
 import { IconeAjustes, IconeInicio, IconeSino } from "../icons";
+import Logo from "./Logo";
 import { MenuItem } from "./MenuItem";
 
 export function MenuLateral() {
   return (
     <aside>
+      <div
+        className={`
+          flex flex-col items-center justify-center
+          bg-gradient-to-tr from-indigo-500 via-blue-600 to-purple-800
+          h-20 w-20
+      `}
+      >
+        <Logo />
+      </div>
       <ul>
         <MenuItem url="/" texto="Início" icone={IconeInicio} />
         <MenuItem url="/ajustes" texto="Ajustes" icone={IconeAjustes} />
--- src/components/template/Titulo.tsx
+++ src/components/template/Titulo.tsx
@@ -8,15 +8,16 @@ export function Titulo(props: TituloProps) {
     <div>
       <h1
         className={`
-
-      `}
+          font-black text-3xl
+          text-gray-900
+        `}
       >
         {props.titulo}
       </h1>
       <h2
         className={`
-
-      `}
+          font-light text-sm text-gray-600
+        `}
       >
         {props.subtitulo}
       </h2>
